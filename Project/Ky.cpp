e92c9acf5e8a8ce9e01512729a4a7f0648e7e98c
Nguyễn Hoàng Kỳ <nhktmdzhg@gmail.com>
2023-06-28 10:19:19 +0700

    wite to file and update EngVie dataset

--- Project/Ky.cpp
+++ Project/Ky.cpp
@@ -1,6 +1,6 @@
-#include "function.h"
+#include"function.h"
 
-void delete(trieNode*& root, string word, int depth) {
+void DeleteAWord(trieNode*& root, string word, int depth) {
 	if (root == nullptr)
 		return;
 	if (depth == word.size()) {
@@ -21,7 +21,7 @@ void delete(trieNode*& root, string word, int depth) {
 		return;
 	}
 	int index = word[depth] - 32;
-	delete(root->c[index], word, depth + 1);
+	DeleteAWord(root->c[index], word, depth + 1);
 	bool hasChild = false;
 	for (trieNode* i : root->c)
 		if (i != nullptr) {
@@ -32,4 +32,33 @@ void delete(trieNode*& root, string word, int depth) {
 		delete root;
 		root = nullptr;
 	}
+}
+
+void DeleteAllTree(trieNode*& root) {
+	if (root == nullptr)
+		return;
+	for (int i = 0; i < alphabet; i++)
+		DeleteAllTree(root->c[i]);
+	delete root;
+	root = nullptr;
+}
+
+void WriteDictionaryToFile(trieNode* root, string prefix, ofstream& fout) {
+	if (root == nullptr)
+		return;
+	if (root->isEnd) {
+		fout << prefix << "`";
+		int numOfMean = root->mean.size();
+		for (int i = 0; i < numOfMean; ++i) {
+			fout << root->mean[i];
+			if (i != numOfMean - 1)
+				fout << "|";
+		}
+		fout << endl;
+	}
+	for (int i = 0; i < alphabet; i++)
+		if (root->c[i] != nullptr) {
+			char temp = i + 32;
+			WriteDictionaryToFile(root->c[i], prefix + temp, fout);
+		}
 }
--- Project/function.h
+++ Project/function.h
@@ -3,8 +3,17 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include <fstream>
 
 using namespace std;
 
 void insert(trieNode*& root, string s, vector<string> meaning);
 void standardWord(string& S);
+
+void insert(trieNode*& root, string s, string meaning);
+
+void DeleteAWord(trieNode*& root, string word, int depth);
+
+void DeleteAllTree(trieNode*& root);
+
+void WriteDictionaryToFile(trieNode* root, string prefix, ofstream &fout);
