56e709427ff6f78b18c91e82f851161a919cc273
Huynh Phan Nhat Vy <71034074+nhatvy118@users.noreply.github.com>
2023-06-28 18:44:48 +0700

    read file

--- Project/Trie.cpp
+++ Project/Trie.cpp
@@ -3,6 +3,7 @@
 #include"struct.h"
 #include<string>
 #include<vector>
+#include<fstream>
 
 using namespace std;
 
@@ -30,4 +31,29 @@ void insert(trieNode*& root, string s, vector<string> meaning) {
 		standardWord(x);
 		cur->mean.push_back(x);
 	}
+}
+void readFileToTree(trieNode* root, string direction) {
+	ifstream fin;
+	fin.open("../Dataset/" + direction);
+	string s = "";
+	while (getline(fin, s)) {
+		string str = s.substr(0, s.find('`'));
+		vector<string> meaning;
+		int i = s.find('`') + 1;
+		int sz = s.size();
+		meaning.resize(0);
+		string tmp = "";
+		while (i < s.size()){
+			if (s[i]!='|') tmp += s[i];
+			else {
+				if (tmp.size() != 0) {
+					meaning.push_back(tmp);
+					tmp = "";
+				}
+			}
+			i++;
+		}
+		insert(root, str,meaning);
+	}
+	fin.close();
 }
